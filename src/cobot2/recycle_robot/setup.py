0e30bd4fa171d87083d60f16971c32f27aa8df77
jooeun6976-rgb <jooeun6976@gmail.com>
2026-09-07 09:41:52 +0900

    build: register recycling ROS2 nodes

---
+++ src/cobot2/recycle_robot/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+package_name = 'recycle_robot'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        (
+            'share/ament_index/resource_index/packages',
+            ['resource/' + package_name],
+        ),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='rokey',
+    maintainer_email='rokey@example.com',
+    description='ROS2 task manager and conveyor control for Rocycle',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'task_manager_node = recycle_robot.task_manager_node:main',
+            'conveyor_node = recycle_robot.conveyor_node:main',
+        ],
+    },
+)
